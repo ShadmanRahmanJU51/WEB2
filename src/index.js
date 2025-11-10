e8a4442da9f0f7cf550f62422c2b1100a92887a3
ShadmanRahmanJU51 <shadman.rahman@gmail.com>
2025-11-10 23:11:11 +0600

    Assignment 2: Add GET /health route with uptime

--- src/index.js
+++ src/index.js
@@ -13,4 +13,11 @@ const tasks = [
 app.get('/', (req, res) => res.send('Task Management API is running!'));
 app.get('/tasks', (req, res) => res.json(tasks));
 
+app.get('/health', (req, res) => {
+  res.json({
+    status: "healthy",
+    uptime: process.uptime()
+  });
+});
+
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
